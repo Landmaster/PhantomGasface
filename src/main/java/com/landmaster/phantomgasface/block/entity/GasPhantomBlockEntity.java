9a80ca56adc47a8d8b597ee1f9d869911b3309d2
Landmaster <python0429@gmail.com>
2025-12-06 21:17:24 -0500

    fix block drops

--- src/main/java/com/landmaster/phantomgasface/block/entity/GasPhantomBlockEntity.java
+++ src/main/java/com/landmaster/phantomgasface/block/entity/GasPhantomBlockEntity.java
@@ -7,7 +7,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class GasPhantomBlockEntity extends TileEntityPhantomface {
